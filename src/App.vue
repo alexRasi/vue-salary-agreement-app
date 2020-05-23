e630246cbe87b1f67e2648ecd54559a648611919
a.rasidakis <a.rasidakis@interfacema.de>
2020-05-23 16:10:27 +0200

    Remove default initialization code and add the empty tabs window component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TabsWindow msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TabsWindow from './components/TabsWindow.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TabsWindow
   }
 }
 </script>
 
 <style>
+body {
+  margin:0;
+  padding:0;
+  width: 100vw;
+  height: 100vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TabsWindow.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  tabs window
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tabs-window',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
