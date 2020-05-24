053055de8da50521d95efcb62f2cabaf1714cf13
a.rasidakis <a.rasidakis@interfacema.de>
2020-05-24 13:37:36 +0200

    Use TabsNavbar and Tabs componnets inside the TabsWindow component

--- src/components/TabsWindow.vue
+++ src/components/TabsWindow.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
-  tabs window
+  <div class="tabs-window">
+  <TabsNavbar class="tabs-navbar">
+    <TabNav title="Employer" active="true"/>
+    <TabNav title="Employee"/>
+  </TabsNavbar>
   </div>
 </template>
 
 <script>
+import TabNav from './TabNav.vue'
+import TabsNavbar from './TabsNavbar.vue'
+
 export default {
   name: 'tabs-window',
+  components: {
+    TabNav,
+    TabsNavbar
+  },
   props: {
     msg: String
   }
@@ -14,5 +24,12 @@ export default {
 </script>
 
 <style scoped>
-
+  .tabs-window {
+    width: 50vw;
+    height: 30vh;
+    border: 1px solid lightgray;
+    margin-bottom: 30%;
+    box-shadow: 2px 6px 12px #00000059;
+    display: flex;
+  }
 </style>
