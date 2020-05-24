942d16478c5eba10a5b235991eb00c1e153b8d41
a.rasidakis <a.rasidakis@interfacema.de>
2020-05-24 13:36:57 +0200

    Create TabsNavbar component

---
+++ src/components/TabsNavbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tabs-navbar" >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'tabs-navbar',
+}
+</script>
+
+<style scoped>
+  .tabs-navbar {
+    width: 100%;
+    height: 15%;
+    background-color: gray;
+    display: flex;
+  }
+</style>
